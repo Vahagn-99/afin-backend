19fcd7f32d165e7113306a3c09f205b3b98e1d9d
Vahagn <vahagn99ghukasyan@gmail.com>
2024-07-18 21:31:28 +0400

    fix horizon: view

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -10,10 +10,18 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
     public function boot(): void
     {
+        parent::boot();
     }
 
 
     protected function gate(): void
     {
+    }
+
+    protected function authorization(): void
+    {
+        Horizon::auth(function () {
+            return true;
+        });
     }
 }
