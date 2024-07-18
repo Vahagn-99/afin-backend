0324e05e1b52ae57628ca521329f1c6aa7a9fb4d
Vahagn <vahagn99ghukasyan@gmail.com>
2024-07-19 00:41:33 +0400

    add: webhook reloader

--- app/Services/Syncer/Config/Config.php
+++ app/Services/Syncer/Config/Config.php
@@ -8,8 +8,8 @@ interface Config
     const ANALYTIC_FIELD_ID = 499463;
     const DEFAULT_BRANCH_NAME = "Отдел продаж";
 
-    const CONTACT_ADDED_WEBHOOK = "add_lead";
-    const CONTACT_UPDATED_WEBHOOK = "update_lead";
+    const CONTACT_ADDED_WEBHOOK = "add_contact";
+    const CONTACT_UPDATED_WEBHOOK = "update_contact";
     const RATING_PIPELINE_ID = 8390486;
     const RATING_STATUSES_ID = [
         142
