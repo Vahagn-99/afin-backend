d487dd1f9a413d9c6e5065da01f5c1452986e073
Vahagn <vahagn99ghukasyan@gmail.com>
2024-07-19 00:06:35 +0400

    add: reset admin command

---
+++ app/Console/Commands/ResetAdminCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class ResetAdminCommand extends Command
+{
+    protected $signature = 'reset:admin';
+
+    public function handle()
+    {
+        $email = null;
+        $password = Str::random(8);
+        do {
+            $email = $this->ask('What is your email?');
+        } while (is_null($email));
+
+        /** @var User $admin */
+        $admin = User::query()->first();
+        $admin->password = bcrypt($password);
+        $admin->email = $email;
+        $admin->save();
+        $this->info('Admin reset successfully. email: ' . $email . ' password: ' . $password);
+    }
+}
